6b5ae587b84d24bbb02aee7e8c7c3b672090e9bc
iannakim <dear.hyunji@gmail.com>
2020-09-20 20:46:40 -0400

    indent for organization

--- src/index.js
+++ src/index.js
@@ -18,72 +18,69 @@ fetch(FilmUrl, { method: "GET"})
         const titleLi = allFilms.map((movie) => `<div class="film item" id="${movie.id}">${movie.title}</div>`)
         movieDiv.innerHTML = titleLi.join('')
         
- 
+
 
         movieDiv.addEventListener("mouseover", (event) => {   
             event.target.style.color = "orange";
             setTimeout(function() {
-              event.target.style.color = "";
+            event.target.style.color = "";
             }, 300);
-          }, false);
+        }, false);
 
 
         movieDiv.addEventListener('click', (event) => {
             let selectedFilmId = event.target.id
             loadFilm(selectedFilmId)
-
         })
 
 
     })
 
 
-function loadFilm(filmId) {
-fetch(`http://localhost:3000/films/${filmId}`, { method: "GET" })
-    .then(res => res.json())
-    .then((filmPOJO) => {
-        const film = filmPOJO
-        posterDiv.src = film.poster
-        titleDiv.innerText = film.title
-        runtimeDiv.innerText = `${film.runtime} minutes`
-        infoDiv.innerText = film.description
-        showtimeSpan.innerText = film.showtime
-        ticketSold = film.tickets_sold
-        ticketLeft = film.capacity - film.tickets_sold
-        ticketNumSpan.innerText = `${ticketLeft}`
-        if (ticketLeft == 0) {
-            ticketBtn.className = "ui label"
-            ticketBtn.innerText = "SOLD OUT"
-        }
-    })
+    function loadFilm(filmId) {
+    fetch(`http://localhost:3000/films/${filmId}`, { method: "GET" })
+        .then(res => res.json())
+        .then((filmPOJO) => {
+            const film = filmPOJO
+                posterDiv.src = film.poster
+                titleDiv.innerText = film.title
+                runtimeDiv.innerText = `${film.runtime} minutes`
+                infoDiv.innerText = film.description
+                showtimeSpan.innerText = film.showtime
 
-    ticketBtn.addEventListener('click', (evt) => {
-        if (ticketLeft > 0) {
-            ticketLeft = ticketLeft - 1
-            ticketSold = ticketSold + 1
-            fetch(`http://localhost:3000/films/${filmId}`, {
-                method: "PATCH",
-                headers: {
-                    "content-type": "application/json"
-                },
-                body: JSON.stringify({
-                    tickets_sold: ticketSold
-                })
-            })
-            .then(res => res.json())
-            .then((updatedObj) => {
-                ticketSold = updatedObj.tickets_sold
-                ticketLeft = updatedObj.capacity - updatedObj.tickets_sold
+                ticketSold = film.tickets_sold
+                ticketLeft = film.capacity - film.tickets_sold
                 ticketNumSpan.innerText = `${ticketLeft}`
+
+                    if (ticketLeft == 0) {
+                        ticketBtn.className = "ui label"
+                        ticketBtn.innerText = "SOLD OUT"
+                    }
+        })
+
+            ticketBtn.addEventListener('click', (evt) => {
+                if (ticketLeft > 0) {
+                    ticketLeft = ticketLeft - 1
+                    ticketSold = ticketSold + 1
+                    fetch(`http://localhost:3000/films/${filmId}`, {
+                        method: "PATCH",
+                        headers: {"content-type": "application/json"},
+                        body: JSON.stringify({tickets_sold: ticketSold})
+                    })
+                    .then(res => res.json())
+                    .then((updatedObj) => {
+                        ticketSold = updatedObj.tickets_sold
+                        ticketLeft = updatedObj.capacity - updatedObj.tickets_sold
+                        ticketNumSpan.innerText = `${ticketLeft}`
+                    })
+                
+                    
+                }
+                else if (ticketLeft == 0) {
+                    ticketBtn.className = "ui label"
+                    ticketBtn.innerText = "SOLD OUT"
+                }
             })
-        
-            
-        }
-        else if (ticketLeft == 0) {
-            ticketBtn.className = "ui label"
-            ticketBtn.innerText = "SOLD OUT"
-        }
-    })
 }
 
 
