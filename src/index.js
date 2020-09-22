3d8fcdc070f6a9f4fd7f0902f1365d6cb5a2f748
ChristyTropila <tropila04@gmail.com>
2020-09-22 18:04:57 -0400

    Finished listing out all movies and the buy ticket click works

--- src/index.js
+++ src/index.js
@@ -12,6 +12,10 @@ const filmInfo=document.querySelector('#film-info')
 const showTime=document.querySelector('#showtime')
 const tickets=document.querySelector('#ticket-num')
 const button=document.querySelector('.ui.orange.button')
+const topOfSideBar=document.querySelector(".film.item")
+const filmItem=document.querySelector('div.film.item:nth-child(2)')
+let filmList= " "
+
 
 
 fetch(url)
@@ -24,16 +28,19 @@ fetch(url)
     showTime.innerText=movieArray[0].showtime
 
     tickets.innerText=(movieArray[0].capacity-movieArray[0].tickets_sold)
-    let ticketCount=(movieArray[0].capacity-movieArray[0].tickets_sold)
+         buyTicketFunc(movieArray[0])
+})
 
-  
-button.addEventListener("click",  (e) => {
+
+//helper function 
+  function buyTicketFunc(movie) {
+    button.addEventListener("click",  (e) => {
         e.preventDefault()
-       let currentTics=movieArray[0].tickets_sold +1
-       let cap=movieArray[0].capacity
+       let currentTics=movie.tickets_sold +1
+       let cap=movie.capacity
    
     
-fetch(`${url}/${movieArray[0].id}`, {
+fetch(`${url}/${movie.id}`, {
         method: 'PATCH',
         headers: {
             "Content-Type": "application/json"
@@ -47,15 +54,69 @@ fetch(`${url}/${movieArray[0].id}`, {
         tickets.innerText=cap-updatedTickets.tickets_sold
         let newValue=(cap-updatedTickets.tickets_sold)
      if(newValue>0){
-        movieArray[0].tickets_sold=updatedTickets.tickets_sold
+        movie.tickets_sold=updatedTickets.tickets_sold
      }else{
         button.innerText="Sold OUT"
      }
+    })   
+})
+
+} 
+
+
+//bonus deliverables
+fetch(url)
+.then(resp => resp.json())
+.then(movieArray => {
+    filmItem.innerHTML=" "
+    topOfSideBar.innerHTML= " "
+    movieArray.forEach(movie => {
+        let filmLi=document.createElement("li")
+        filmLi.innerText=movie.title
+        filmItem.append(filmLi)
+         
+
+        filmLi.addEventListener("click", (e)=> {
+     
+            movieTitle.innerText= movie.title
+            poster.src=movie.poster
+            runTime.innerText=`${movie.runtime} minutes`
+            filmInfo.innerText=movie.description
+            showTime.innerText=movie.showtime
+            tickets.innerText=(movie.capacity-movie.tickets_sold)
+            buyTicketFunc(movie)
+             
+        //       button.addEventListener("click",  (e) => {
+        //         e.preventDefault()
+        //        let currentTics=movie.tickets_sold +1
+        //        let cap=movie.capacity
+           
+            
+        // fetch(`${url}/${movie.id}`, {
+        //         method: 'PATCH',
+        //         headers: {
+        //             "Content-Type": "application/json"
+        //         },
+        //         body: JSON.stringify({
+        //             tickets_sold: currentTics
+        //         })
+        //     })
+        //  .then(resp => resp.json())
+        //  .then(updatedTickets =>{
+        //         tickets.innerText=cap-updatedTickets.tickets_sold
+        //         let newValue=(cap-updatedTickets.tickets_sold)
+        //      if(newValue>0){
+        //         movie.tickets_sold=updatedTickets.tickets_sold
+        //      }else{
+        //         button.innerText="Sold OUT"
+        //      }
+        //     })
+            
+        // })
+        
     })
-    
+  
+ })
+
+
 })
-})
-
-
-
-
