53c15abde46661ff6cd95d6ceb5483387c8da3a0
wilsonng21 <wilsongch92@gmail.com>
2020-09-22 17:28:13 -0400

    first two adv deliverables working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+// Please read notes before testing:
+
+// tried to implement buying ticket function for all movies.
+// issue - when clicking buy ticket button on a movie OTHER THAN first movie
+// -it updates the first movie's ticket amount left.
+// couldn't figure out how to refactor in order to remove
+// -event listener on the button, it is always associated with the first movie
+// issue - buy ticket button also randomly associates with other movies
+// maybe not having the button as a stable element would be a workaround in this scenario?
+
+// Core deliverables working and two advanced below working:
+// See a menu of all movies on the left side of the page.
+// Click on a movie in the menu to replace the currently displayed movie's details with the new movie's details.
+
+// Advanced deliverables NOT working:
+// Buy a ticket for any movie and update the tickets sold for that movie, not just the first.
+// Indicate in the menu which movies are sold out.
+
+
 const url = "http://localhost:3000/films"
 
 const filmPoster = document.querySelector('#poster')
@@ -7,6 +26,7 @@ const filmRuntime = document.querySelector('div#runtime')
 const filmShowtime = document.querySelector('span#showtime')
 const ticketNum = document.querySelector('span#ticket-num')
 const buyTicketBtn = document.querySelector('div.ui.orange.button')
+const movieList = document.querySelector('#films')
 
 const calculateTicketsLeft = film => parseInt(film.capacity, 10) - film.tickets_sold
 
@@ -19,12 +39,12 @@ const parseOneFilm = film => {
     filmShowtime.innerText = film.showtime
     ticketNum.innerText = ticketsLeft
 
-    if (ticketsLeft === 0) {
+    if (!ticketsLeft) {
         buyTicketBtn.remove()
     }
 }
 
-const buyTicket = (film) => {
+const buyTicket = film => {
     buyTicketBtn.addEventListener('click', event => {
         let newTicketsSold = film.tickets_sold + 1
         fetch(`${url}/${film.id}`, {
@@ -46,14 +66,32 @@ const buyTicket = (film) => {
     })
 }
 
-// get first film
-const getFirstFilm = () => {
-    fetch(`${url}/1`)
-        .then(response => response.json())
-        .then(film => {
-            parseOneFilm(film)
-            buyTicket(film)
-        })
-}
+// getfirst film
 
-getFirstFilm()
+fetch(`${url}/1`)
+    .then(response => response.json())
+    .then(film => {
+        parseOneFilm(film)
+        buyTicket(film)
+        getAllFilms()
+    })
+
+const getAllFilms = () => {
+    fetch(url)
+        .then(response => response.json())
+        .then(filmsArray => {
+
+            filmsArray.forEach(film => {
+                let filmList = document.createElement('div')
+                filmList.classList.add('film', 'item')
+                filmList.innerText = film.title
+                movieList.append(filmList)
+
+                filmList.addEventListener('click', event => {
+                    parseOneFilm(film)
+                    // buyTicket(film)
+                })
+            })
+        })
+
+}
