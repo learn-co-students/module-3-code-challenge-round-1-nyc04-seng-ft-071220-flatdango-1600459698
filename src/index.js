d5f610a1c60ecf00730184754c68543dc2e9b14c
wlcreate <waverley.nyc@gmail.com>
2020-09-22 16:50:59 -0400

    finishes all core and advanced deliverables with styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const url = "http://localhost:3000/films"
 
 let moviePoster = document.querySelector("img#poster")
+let movieInfoCard = document.querySelector("div#showing")
 let movieTitle = document.querySelector("div#title")
 let movieRuntime = document.querySelector("div#runtime")
 let movieDesc = document.querySelector("div#film-info")
@@ -11,33 +12,41 @@ let buyTicket = document.querySelector("div.ui.orange.button")
 let movieMenuMainDiv = document.querySelector("div.four.wide.column")
 let movieMenuContainer = document.querySelector("div.list-container")
 let movieMenuList = document.querySelector("div#films")
-// let movieMenuItem = document.querySelector("div.film.item")
+
+let globalMovie = {}
 
 // First Core Deliverable
-// fetch("http://localhost:3000/films/1")
-// .then(response => response.json())
-// .then(firstMovieObj => {
-//     displayMovieInfo(firstMovieObj)
-// })
+fetch(url + "/1")
+.then(response => response.json())
+.then(firstMovieObj => {
+    displayMovieInfo(firstMovieObj)
+})
 
+// This is getting the Sidebar Info
 fetch(url)
 .then(response => response.json())
 .then(movieArray => {
-    // clears the list first
+    // clears the side bar list
     while (movieMenuList.firstChild) {
         movieMenuList.firstChild.remove()
     }
-
+    // for each movie, display it on the sidebar
     movieArray.forEach(movieObj => {
         turnMovieToDiv(movieObj)
     })
 })
 
+// This takes each movie's info and adds it to the sidebar as a div
 let turnMovieToDiv = (movie) => {
     let movieDiv = document.createElement("div")
-    movieDiv.className = "film item"
     movieDiv.innerText = movie.title
 
+    if (findRemainingTickets(movie.capacity, movie.tickets_sold) === 0) {
+        movieDiv.className = "sold-out film item"
+    } else {
+        movieDiv.className = "film item"
+    }
+    
     movieMenuList.append(movieDiv)
 
     movieDiv.addEventListener("click", (evt) => {
@@ -45,52 +54,88 @@ let turnMovieToDiv = (movie) => {
     })
 }
 
-let displayMovieInfo = (movie) => {
-    moviePoster.src = movie.poster
-    movieTitle.innerText = movie.title
-    movieRuntime.innerText = `${movie.runtime} minutes`
-    movieDesc.innerText = movie.description
-    movieShowtime.innerText = movie.showtime
+// Displaying Movie Info
+let displayMovieInfo = (selectedMovie) => {
 
-    let tickets = findRemainingTickets(movie.capacity, movie.tickets_sold)
-    movieRemainingTickets.innerText = tickets
-    // debugger
-    buyTicket.addEventListener("click", (evt) => {
-        // debugger
-        if (tickets > 0) {
-            let boughtTicket = movie.tickets_sold + 1
-            //updates the backend
-            fetch(`http://localhost:3000/films/${movie.id}`, {
-                method: "PATCH",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    tickets_sold: boughtTicket
-                })
-            })
-            .then(response => response.json())
-            .then(updatedMovieObj=> {
-                //updates the object in memory
-                movie.tickets_sold = updatedMovieObj.tickets_sold
+    globalMovie = selectedMovie
 
-                //updates the DOM
-                tickets = findRemainingTickets(updatedMovieObj.capacity, updatedMovieObj.tickets_sold)
-                movieRemainingTickets.innerText = tickets
+    moviePoster.src = selectedMovie.poster
+    movieTitle.innerText = selectedMovie.title
+    movieRuntime.innerText = `${selectedMovie.runtime} minutes`
+    movieDesc.innerText = selectedMovie.description
+    movieShowtime.innerText = selectedMovie.showtime
 
-                if (tickets === 0) {
-                    buyTicket.innerText = "Sold Out"
-                    buyTicket.className = "ui label"
-                }
-            })
-        } else if (tickets === 0) {
-            buyTicket.innerText = "Sold Out"
-            buyTicket.className = "ui label"
-        }
-    })
+    movieRemainingTickets.innerText = findRemainingTickets(selectedMovie.capacity, selectedMovie.tickets_sold)
+
+    let selectedMovieIdNum = parseInt(selectedMovie.id)
+    let selectedMovieDiv = document.querySelector("#films > .film.item:nth-child(" + selectedMovieIdNum + ")")
+    if (selectedMovieDiv.className === "sold-out film item") {
+        buyTicket.innerText = "Sold Out"
+        buyTicket.className = "ui label"
+    } else {
+        buyTicket.className = "ui orange button"
+        buyTicket.innerText = "Buy Ticket"
+    }
 
 }
 
+// Clicking on the "Buy Ticket" Button
+buyTicket.addEventListener("click", (evt) => {
+    // should not be able to buy a ticket if the movie is sold out
+    debugger
+    if (parseInt(movieRemainingTickets.innerText) > 0) {
+        let boughtTicket = globalMovie.tickets_sold + 1
+        //updates the backend
+        fetch(`http://localhost:3000/films/${globalMovie.id}`, {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                tickets_sold: boughtTicket
+            })
+        })
+        .then(response => response.json())
+        .then(updatedMovieObj=> {
+            //updates the object in memory
+            globalMovie.tickets_sold = updatedMovieObj.tickets_sold
+
+            //updates the DOM
+            movieRemainingTickets.innerText = findRemainingTickets(updatedMovieObj.capacity, updatedMovieObj.tickets_sold)
+            debugger
+
+            if (parseInt(movieRemainingTickets.innerText) === 0) {
+                let updatedMovieIdNum = parseInt(updatedMovieObj.id)
+                let updatedMovieDiv = document.querySelector("#films > .film.item:nth-child(" + updatedMovieIdNum + ")")
+                updatedMovieDiv.className = "sold-out film item"
+
+                buyTicket.innerText = "Sold Out"
+                buyTicket.className = "ui label"
+
+            }
+        })
+    } 
+})
+
+// helper method to find out how many tickets are left
 let findRemainingTickets = (theaterCapacity, soldTickets) => {
     return theaterCapacity - soldTickets
 }
+// end of helper method
+
+// helper method for when a movie becomes sold out
+// let soldOutDOM = (soldOutMovie) => {
+
+//     debugger
+
+//     let updatedMovieDiv = document.querySelector("div.film.item")
+//     updatedMovieDiv.className = "sold-out film item"
+
+//     buyTicket.innerText = "Sold Out"
+//     buyTicket.className = "ui label"
+// }
+
+// How to find specific div on the side bar
+// document.querySelector("#films > .film.item:nth-child(5)")
+// let selectedMovieIdNum = parseInt(selectedMovie.id)
+// selectedMovieDiv = document.querySelector("#films > .film.item:nth-child(" + selectedMovieIdNum + ")")
