dfc8d4f2aae3f7fea57f1aef43831e1a35f71f16
iannakim <dear.hyunji@gmail.com>
2020-09-22 12:30:21 -0400

    side bar changes color upon sold out

--- src/index.js
+++ src/index.js
@@ -1,87 +1,125 @@
-const FilmUrl = "http://localhost:3000/films"
+
+const filmUrl = "http://localhost:3000/films"
+
+//SIDE BAR FOR MOVIE TITLES
+const movieDiv = document.querySelector("div#films")
+const defaultMovieDiv2 = document.querySelector("div.film.item.film.item:nth-child(2)").remove()
+const defaultMovieDiv1 = document.querySelector("div.film.item.film.item:nth-child(1)").remove()
+
+//MOVIE POSTER WILL APPEAR HERE
 const posterDiv = document.querySelector("img#poster")
+
+//SELECTED MOVIE INFORMATION
 const titleDiv = document.querySelector("div#title.title")
 const runtimeDiv = document.querySelector("div#runtime.meta")
 const infoDiv = document.querySelector("div#film-info")
 const showtimeSpan = document.querySelector("span#showtime.ui.label")
 const ticketNumSpan = document.querySelector("span#ticket-num")
 const ticketBtn = document.querySelector("div.ui.orange.button")
-const movieDiv = document.querySelector("div#films.ui.divided.list")
+
+//HELPER GLOBAL VARIABLES
 let ticketLeft = 0
 let ticketSold = 0
 let allFilms = []
+let film = {}
 
-fetch(FilmUrl, { method: "GET"})
+////////////////////////////////////////////////////////////////////////
+
+fetch(filmUrl, { method: "GET"})
     .then(res => res.json())
     .then((filmsPOJO) => {
         allFilms = filmsPOJO
-        const titleLi = allFilms.map((movie) => `<div class="film item" id="${movie.id}">${movie.title}</div>`)
-        movieDiv.innerHTML = titleLi.join('')
-        
-
+        createTitles(allFilms)
 
         movieDiv.addEventListener("mouseover", (event) => {   
-            event.target.style.color = "orange";
+            event.target.style.color = "#3366FF";
             setTimeout(function() {
             event.target.style.color = "";
             }, 300);
         }, false);
 
+    })
 
-        movieDiv.addEventListener('click', (event) => {
-            let selectedFilmId = event.target.id
-            loadFilm(selectedFilmId)
+////////////////////////////////////////////////////////////////////////
+
+let createTitles = (allMovies) => {
+    allMovies.forEach((movie) =>{
+    const titleLi = document.createElement("div") 
+
+        if (movie.capacity - movie.tickets_sold == 0) {
+            
+            titleLi.className = "sold-out film item"
+            titleLi.innerText = movie.title
+            titleLi.id = movie.id
+        }
+        else { 
+            titleLi.className = "film item"
+            titleLi.innerText = movie.title
+            titleLi.id = movie.id
+        }
+   
+        movieDiv.append(titleLi)
+
+        titleLi.addEventListener('click', (event) => {
+            loadFilm(titleLi.id)
         })
 
+    })
+}
 
+///////////////////////////////////////////////////////////////////////
+
+    function loadFilm(filmId) {
+        fetch(`http://localhost:3000/films/${filmId}`, { method: "GET" })
+        .then(res => res.json())
+        .then((filmPOJO) => {
+            film = filmPOJO
+            posterDiv.src = film.poster
+            titleDiv.innerText = film.title
+            runtimeDiv.innerText = `${film.runtime} minutes`
+            infoDiv.innerText = film.description
+            showtimeSpan.innerText = film.showtime
+
+            ticketSold = film.tickets_sold
+            ticketLeft = film.capacity - film.tickets_sold
+            ticketNumSpan.innerText = `${ticketLeft}`
+
+            if (ticketLeft > 0) {
+                ticketBtn.className = "ui orange button"
+                ticketBtn.innerText = "Buy Ticket"
+            }
+            else {
+                ticketBtn.className = "ui label"
+                ticketBtn.innerText = "SOLD OUT"
+            }
+        })
+    }
+
+    ticketBtn.addEventListener('click', (evt) => {
+        if (ticketLeft > 0) {
+            ticketLeft = ticketLeft - 1
+            ticketSold = ticketSold + 1
+            fetch(`http://localhost:3000/films/${film.id}`, {
+                method: "PATCH",
+                headers: {"content-type": "application/json"},
+                body: JSON.stringify({tickets_sold: ticketSold})
+            })
+            .then(res => res.json())
+            .then((updatedObj) => {
+                ticketSold = updatedObj.tickets_sold
+                ticketLeft = updatedObj.capacity - updatedObj.tickets_sold
+                ticketNumSpan.innerText = `${ticketLeft}`
+            })
+        }
+        if (ticketLeft == 0) {
+            ticketBtn.className = "ui label"
+            ticketBtn.innerText = "SOLD OUT"
+            
+        }
     })
 
 
-    function loadFilm(filmId) {
-    fetch(`http://localhost:3000/films/${filmId}`, { method: "GET" })
-        .then(res => res.json())
-        .then((filmPOJO) => {
-            const film = filmPOJO
-                posterDiv.src = film.poster
-                titleDiv.innerText = film.title
-                runtimeDiv.innerText = `${film.runtime} minutes`
-                infoDiv.innerText = film.description
-                showtimeSpan.innerText = film.showtime
-
-                ticketSold = film.tickets_sold
-                ticketLeft = film.capacity - film.tickets_sold
-                ticketNumSpan.innerText = `${ticketLeft}`
-
-                    if (ticketLeft == 0) {
-                        ticketBtn.className = "ui label"
-                        ticketBtn.innerText = "SOLD OUT"
-                    }
-        })
-
-            ticketBtn.addEventListener('click', (evt) => {
-                if (ticketLeft > 0) {
-                    ticketLeft = ticketLeft - 1
-                    ticketSold = ticketSold + 1
-                    fetch(`http://localhost:3000/films/${filmId}`, {
-                        method: "PATCH",
-                        headers: {"content-type": "application/json"},
-                        body: JSON.stringify({tickets_sold: ticketSold})
-                    })
-                    .then(res => res.json())
-                    .then((updatedObj) => {
-                        ticketSold = updatedObj.tickets_sold
-                        ticketLeft = updatedObj.capacity - updatedObj.tickets_sold
-                        ticketNumSpan.innerText = `${ticketLeft}`
-                    })
-                
-                    
-                }
-                else if (ticketLeft == 0) {
-                    ticketBtn.className = "ui label"
-                    ticketBtn.innerText = "SOLD OUT"
-                }
-            })
-}
 
 
-loadFilm(1) //default page should point to the first movie since the placeholder poster is creepy.
+//DEFAULT PAGE
+loadFilm(1) 
