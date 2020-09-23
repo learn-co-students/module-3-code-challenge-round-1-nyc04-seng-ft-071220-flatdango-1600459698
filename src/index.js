b8aed7585a521802b7031cf398ddffa49a21b9a5
jacinta246 <jacinta246@yahoo.com>
2020-09-23 05:10:36 -0400

    got the films to display buy not in a div

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 const url = "http://localhost:3000/films"
+let titleUl = document.querySelector("div#films")
+
+
+
+
+/// grabbing the objects from the api/json file
+fetch(`http://localhost:3000/films`)
+.then (response => response.json())
+.then((filmsArray) => {
+    filmsArray.forEach((aFilm) => {
+        turnFilmLiToHTML(aFilm)
+    })
+})
+
+let turnFilmLiToHTML = (filmObj) => {
+    
+    let titleLi = document.createElement("li") 
+    titleLi.classList.add("list-container")
+    
+    titleLi.innerText = filmObj.title
+   
+    titleUl.append(titleLi)
+
+   
+
+
+}
+
