84c76a95332846f7901fa3e0996caf9d2c341b2c
jacinta246 <jacinta246@yahoo.com>
2020-09-23 06:48:35 -0400

    the poster is displayed and movie titles show up in the console when logged

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const url = "http://localhost:3000/films"
 let titleUl = document.querySelector("div#films")
-
+let filmImage = document.querySelector("#poster")
 
 
 
@@ -15,15 +15,22 @@ fetch(`http://localhost:3000/films`)
 
 let turnFilmLiToHTML = (filmObj) => {
     
-    let titleLi = document.createElement("li") 
-    titleLi.classList.add("list-container")
-    
-    titleLi.innerText = filmObj.title
+    let sidebarTitleLi = document.createElement("li") 
+    sidebarTitleLi.classList.add("list-container")
+    //added it to the side bar 
+    sidebarTitleLi.innerText = filmObj.title
+   //grabbing the title
+    titleUl.append(sidebarTitleLi)
+    // adding it to the DOM
    
-    titleUl.append(titleLi)
-
-   
-
+    sidebarTitleLi.addEventListener("click", (evt) =>{
+       renderMainFilm(filmObj) 
+    })
 
 }
 
+
+let renderMainFilm = (aFilm) => {
+    console.log("This is a film", aFilm)
+    filmImage.src = aFilm.poster
+}
